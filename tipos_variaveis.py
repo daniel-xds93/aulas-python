f4ab536377032a20a111ce6673dac221c50f19b6
Daniel Xavier <daniel.xds@hotmail.com>
2023-08-15 22:34:59 -0300

    Tipos de variaveis

---
+++ tipos_variaveis.py
@@ -0,0 +1,19 @@
+'''nome = "Daniel"
+
+idade = 30
+
+salario = 1500.45
+
+print(type(nome))
+print(type(idade))
+print(type(salario))'''
+
+n1 = int(input("Digite o primeiro numero: "))
+n2 = float(input("Digite o segundo numero: "))
+
+res = n1 + n2
+
+print(res)
+print(type(n1))
+print(type(n2))
+print(type(res))
