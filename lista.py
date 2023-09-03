70ab77941d0730a8af8d19e6fa6ef4df91779fa6
Daniel Xavier <daniel.xds@hotmail.com>
2023-09-03 00:18:13 -0300

    Primeira parte da lista, criando e inserindo itens

---
+++ lista.py
@@ -0,0 +1,25 @@
+
+minhaLista = ['Python', 3, 'Aprendendo do inicio', 3.8, 'Daniel']
+            #   0     , 1,    2                  , 3  , 4
+            # 
+print(f'posição indicada da lista {minhaLista[2]}')
+
+print('-------------------')
+
+print(minhaLista)
+
+add = 'adicionando texto da variavel'
+
+minhaLista.append(add)
+
+print("Lista atualizada")
+print(minhaLista)
+
+print("----------------------")
+
+add_posicao2 = "Canal"
+
+minhaLista.insert(2, add_posicao2)
+
+print(minhaLista)
+
