d6aadefe8afb92030a85cbe684a2606d7d5e1706
Daniel Xavier <daniel.xds@hotmail.com>
2023-09-14 21:39:20 -0300

    funcções para listas numericas

---
+++ lista_numerica.py
@@ -0,0 +1,19 @@
+valores = [33, 81, 54, 23, 44, 9, 3, 12]
+
+maior = max(valores) # max -> retorna o maior valor da lista
+menor = min(valores) # min -> retorna o menor valor da lista
+soma = sum(valores) # sum -> soma todos os valores da lista
+qtd_itens = len(valores) # len -> retorna a quantidade de itens na lista
+
+# media dos valores da lista
+media = soma / qtd_itens
+
+print(f"maior valor {maior}")
+
+print(f"menor valor {menor}")
+
+print(f"total somado {soma}")
+
+print(f"quantidade de itens {qtd_itens}")
+
+print(f"média de valores {media}")
